b2609cf1a7edee134de134f81e6e2cb79597b8ab
abhiche <abhimail1@gmail.com>
2019-03-07 18:17:27 +0800

    Create userlist and userdetails component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  async componentDidMount() {
+    const response = await axios.get("https://api.github.com/users");
+
+    this.setState({
+      users: response.data
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.users.map(user => {
+          return (
+            <div key={user.id}>
+              <div>{user.login}</div>
+              <img src={user.avatar_url} alt={user.login} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      // .get("https://api.github.com/users/" + this.props.match.params.id)
+      .get("https://api.github.com/users/" + this.props.id)
+      .then(({ data }) => {
+        this.setState({
+          user: data,
+          isLoading: false
+        });
+        console.log(data);
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>{this.state.user.login}</div>
+          <img src={this.state.user.avatar_url} alt={this.state.user.login} />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(<UserList name="Kerwin" />, document.getElementById("root"));
+ReactDOM.render(<UserDetails id="mojombo" />, document.getElementById("root"));
